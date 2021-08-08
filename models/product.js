d88b7b047c8a2988e18a27cc3b415d896cdb22ca
Gulshann <gulshan@zestiot.io>
2021-08-08 20:27:40 +0530

    initial commit

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+  constructor(id, ownerId, title, imageUrl, description, price) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.title;
+    this.price = price;
+  }
+}
---
+++ store/reducers/products.js
@@ -0,0 +1,4 @@
+const intialState = {
+  availableProducts: [],
+  userProducts: [],
+};
